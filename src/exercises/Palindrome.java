f586f14b1c637fb03edd4a503a9308239fb1e4db
jelenam86 <jelena.mat86@gmail.com>
2019-07-07 21:00:26 +0200

    Added code to the Palindrome class

--- src/exercises/Palindrome.java
+++ src/exercises/Palindrome.java
@@ -9,4 +9,19 @@ public class Palindrome {
 	public List<String> loadWords() {
 		return FileHelper.loadFileContentsIntoArrayList("resource/words.txt");
 	}
+
+	public boolean wordExist(String word) {
+		return loadWords().contains(word);
+	}
+
+	public boolean isPalindrome(String word) {
+		String str = new StringBuilder(word).reverse().toString();
+		return wordExist(word) && wordExist(str);
+	}
+
+	public boolean isPhrasePalindrome(String phrase) {
+		String str = phrase.replaceAll("[^a-zA-Z0-9]+", "");
+		String reversed = new StringBuilder(str).reverse().toString();
+		return reversed.equalsIgnoreCase(str);
+	}
 }
